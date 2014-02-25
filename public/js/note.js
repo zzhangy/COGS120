0b9d328d8788ce8f06aa95ab98f763402426bc76
Yalin Zhang <theyalinzhang@gmail.com>
2014-02-25 15:19:00 -0800

    highlighting things

--- public/js/note.js
+++ public/js/note.js
@@ -2,13 +2,14 @@
 var savedColor = "none";
 var savedColorTag = "0";
 var highlightOn = false;
+var lastColored = null;
 
 function current_date() {
             var d = new Date();
-            //return d.toDateString();
-            return (d.getMonth()+1) + "/" +
+            return d.toDateString();
+            /*return (d.getMonth()+1) + "/" +
                         d.getDate() + "/" +
-                        d.getFullYear();
+                        d.getFullYear();*/
 }
 
 function colorToTag () {
@@ -23,8 +24,13 @@ function selectColor(e) {
 	var color = $(e).attr('id');
 	savedColor = color;
 	savedColorTag = colorToTag();
-	console.log(color);
+	if (lastColored != null) {
+		lastColored.style.borderWidth = '1px';
+	}
+	lastColored = e;
 	$(e).css('border-width',"3px");
+
+	$('#myModal1').modal('hide');
 }
 
 
@@ -47,6 +53,13 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+
+function noteKeyPress(e) {
+	if (e.which == 13) {
+		e.preventDefault();
+		addNote(e);
+	}
+}
 function initializePage() {
 	$('#newnote').click(addNote);
 	$('#submitNote').click(submitNewNote);
@@ -57,41 +70,44 @@ function initializePage() {
 				addNote(e);
 		}
 	});
-	/*$('.note').live('keypress', function(e) {
-		if (e.which == 13) {
-			var notes = $('.note');
-			if (notes.length == 0)
-				addNote(e);
-		}
-	});*/
+	$('.note').on('keypress', function(e) {
+		noteKeyPress(e);
+	});
 	// consider for notes too! but need to attach handler when creating
 	$('#addNewButton').click(addNote);
 	$('#deleteTool').click(function() {
 		$('#deleteForm').submit();
 	});
 	$('#date').text(current_date());
-	$('#highlightButton').click(function() {
-		highlightOn = !highlightOn;
-		$(this).children('span').css('color', '#fff');
+	$('#myModal1').on('hide.bs.modal', function () {
+  		highlightOn = $('#hi-on').hasClass('active');
+		$('#colorsButtonIcon').css('color', '#fff');
 		if (highlightOn) {
 			if (savedColor != "none")
-				$(this).children('span').css('color', savedColor);
+				$('#colorsButtonIcon').css('color', savedColor);
 		}
 	});
 	$('.collapse').collapse();
 }
 
+
 function hello(thisdiv) {
 	//thisdiv.style.backgroundColor = 'yellow';
-	if (highlightOn)
-		thisdiv.dataset.tag = savedColorTag;
+	if (highlightOn) {
+		if (thisdiv.dataset.tag != savedColorTag)
+			thisdiv.dataset.tag = savedColorTag;
+		else
+			thisdiv.dataset.tag = "0";
+	}
 }
 
 function addNote(e) {
-	var newnote = $('<li><div contenteditable="true" class="note edit-note" data-tag="0" onclick="hello(this);"></di></li>');
-	//var newnote = $('<li><textarea onkeyup="new resize_input(this);" virtual rows="1" class="note"></textarea></li>');
-	//var newnote = $('<li><input type="text" class="note"></input></li>');
 	var notelist = $('#notelist');
+	var line_num = notelist.children().length;
+
+	var newnote = $('<li><div contenteditable="true" class="note edit-note" data-tag="0" onclick="hello(this);" onkeypress="noteKeyPress(event)"></div>'+
+		'<a href="/delete/'+curr_note_folder+'/'+curr_note+'/'+line_num+'" class="btn-xs deleteNote"><i class="fa fa-times"></i></a></li>');
+	//var newnote = $('<li><div contenteditable="true" class="note edit-note" data-tag="0" onclick="hello(this);" onkeypress="noteKeyPress(event)"></div></li>');
 	notelist.append(newnote);
 
 	$('#notelist li:last-child div').focus();
@@ -110,7 +126,8 @@ function submitNewNote(e) {
 		var note_json = {"tag": tag, "note": note}; // tag is for highlight
 		note_arr.push(note_json);
 	}
-	var date = $('#date').text(); // date\n
+	//var date = $('#date').text(); // date\n
+	var date = current_date();
 	var title = $('#title').val();
 
 	var note_wrapper = {
--- routes/index.js
+++ routes/index.js
@@ -97,10 +97,12 @@ exports.generateRead = function(req, res){
 
 function current_date() {
             var d = new Date();
-            //return d.toDateString();
+            return d.toDateString();
+            /*
             return (d.getMonth()+1) + "/" +
                         d.getDate() + "/" +
                         d.getFullYear();
+                        */
 }
 
 exports.newFolder = function(req, res){
@@ -320,53 +322,3 @@ exports.viewFolders = function(req, res){
                 this.index = Number(value);}}});
 };
 
-exports.searchLines = function(req, res) {
-	var search_string = req.params.color;
-	var found_lines = search(search_string);
-	res.json(found_lines);
-};
-
-exports.showSearch = function(req, res) {
-	res.render('search');
-};
-
-function search(tag_string) {
-	var found_lines = [];
-	var folders = folderJson.folders;
-	var fi = 0;
-	var folder_length = folders.length;
-	for (; fi < folder_length; ++fi) {
-		// folder
-		var folder_obj = folders[fi];
-		var folder_arr = folder_obj.folder;
-		var ni = 0;
-		var notes_length = folder_arr.length;
-		for (; ni < notes_length; ++ni) {
-			var note = folder_arr[ni];
-			var notes_arr = note.notes;
-			var li = 0;
-			var lines_length = notes_arr.length;
-			for (; li < lines_length; ++li) {
-				var note_line = notes_arr[li];
-				if (note_line.tag == tag_string) {
-					//
-					var line_obj = {
-						'note': note_line.note,
-						'id' : {
-							'line': li,
-							'note': ni,
-							'folder': fi
-						},
-						'text' : {
-							'note_name': note.title,
-							'folder_name': folder_obj.folder_name
-						}
-					};
-					found_lines.push(line_obj);
-				}
-			}
-		}
-	}
-	console.log(found_lines);
-	return found_lines;
-}
